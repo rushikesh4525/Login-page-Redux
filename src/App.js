fe7d68f0d6f5881cab6173c92b095f3c67f70bb9
rushikesh4525 <rushikesh4525@gmail.com>
2024-03-11 17:47:59 +0530

    v1.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Login from './Components/Login';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Logout from './Components/Logout';
+
+function App() {
+  const user = useSelector(selectUser);
+
+  return (
+    <div className='App'>{ user ? <Logout/> : <Login/>}</div>
+
+  );
+}
+
+
+export default App;
